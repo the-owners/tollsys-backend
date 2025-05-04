c36985991724307e70f7d2966ac1632f14bd944a
Derwins Ochoa <111207083+derwins8a@users.noreply.github.com>
2025-05-04 02:31:57 +0000

    feat: complete users crud

--- src/users/controller.py
+++ src/users/controller.py
@@ -16,7 +16,48 @@ router = APIRouter(
 @router.post("/", response_model=User, status_code=status.HTTP_201_CREATED, responses={status.HTTP_201_CREATED: {"description": "User created sucessfully.", "model": User}})
 def create_user(user: User, session: SessionDep):
     db_user = User.model_validate(user)
+    # we have to add hashing and stuff later
     session.add(db_user)
     session.commit()
     session.refresh(db_user)
     return db_user
+
+@router.get("/", response_model=list[UserPublic])
+def read_users(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+):
+    users = session.exec(select(User).offset(offset).limit(limit)).all()
+    return users
+
+
+@router.get("/{user_id}", response_model=UserPublic)
+def read_user(user_id: int, session: SessionDep):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+@router.patch("/{user_id}", response_model=UserPublic)
+def update_user(user_id: int, user: UserUpdate, session: SessionDep):
+    db_user = session.get(User, user_id)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    user_data = user.model_dump(exclude_unset=True)
+    db_user.sqlmodel_update(user_data)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
+
+
+@router.delete("/{user_id}")
+def delete_user(user_id: int, session: SessionDep):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    session.delete(user)
+    session.commit()
+    return {"User deleted sucessfully": True}
