ce95206825858a61bdbd305ee3715d9a5d00d5f6
Derwins Ochoa <111207083+derwins8a@users.noreply.github.com>
2025-05-11 18:01:41 +0000

    fix: main no longer recreates db, also clean up database.core from migration responsibilities

--- src/main.py
+++ src/main.py
@@ -1,12 +1,6 @@
 from fastapi import FastAPI
-from .database.core import create_db_and_tables, initialize_first_data, SessionDep
 from .api import register_routes
 
 app = FastAPI()
-
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
-    initialize_first_data() # and tbh, we should use a migration script or sum
 
 register_routes(app)
