763ba6dc76d9829d84489e6cd2f42eecaad114d7
Derwins Ochoa <111207083+derwins8a@users.noreply.github.com>
2025-05-13 00:58:31 +0000

    fix: cors lmao

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from .database.core import create_db_and_tables, initialize_first_data, SessionDep
 from .api import register_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def on_startup():
