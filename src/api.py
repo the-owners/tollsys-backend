3f5e735396280ac5d3c15a092128f662f195fc01
Derwins Ochoa <111207083+derwins8a@users.noreply.github.com>
2025-05-30 19:20:54 +0000

    feat: finish booth, add routing (again, cuz someone decided to wipe this part)

--- src/api.py
+++ src/api.py
@@ -7,6 +7,7 @@ from src.tolls.controller import router as tolls
 from src.permissions.controller import router as permissions_router
 from src.role_permissions.controller import router as role_permissions_router
 from src.roles.controller import router as roles_router
+from src.booths.controller import router as booths_router
 
 
 def register_routes(app: FastAPI):
@@ -18,3 +19,4 @@ def register_routes(app: FastAPI):
     app.include_router(permissions_router)
     app.include_router(role_permissions_router)
     app.include_router(roles_router)
+    app.include_router(booths_router)
--- src/booths/models.py
+++ src/booths/models.py
@@ -1,3 +1,4 @@
+from typing import Sequence
 import datetime
 import decimal
 from pydantic import BaseModel
@@ -24,14 +25,16 @@ class BoothBase(SQLModel):
 
 class Booth(BoothBase, table=True):
 
-    __tablename__ = "Booth"
+    __tablename__: str = "Booth"
 
     id: int | None = Field(default=None, primary_key=True)
     name: str | None
     active: bool | None
-    status: BoothStatus | None = Field(sa_type=sa.Enum(BoothStatus))
+    status: BoothStatus | None = Field(sa_type=sa.Enum(BoothStatus))  # type: ignore
     toll_id: int | None = Field(foreign_key="Toll.id")
-    created_at: datetime.datetime | None = Field(default_factory=lambda: datetime.now())
+    created_at: datetime.datetime | None = Field(
+        default_factory=lambda: datetime.datetime.now()
+    )
     created_by: int | None = Field(default=None, foreign_key="User.id")
     updated_at: datetime.datetime | None = Field(
         default=None, sa_column=Column(DateTime(), onupdate=func.now())
@@ -67,24 +70,26 @@ class Metadata(SQLModel):
 
 class BoothResponse(BaseModel):
     metadata: Metadata
-    data: list[BoothPublic]
+    data: Sequence[BoothPublic]
     description: str | None = None
 
 
 class BoothCashSession(SQLModel, table=True):
 
-    __tablename__ = "BoothCashSession"
+    __tablename__ = "BoothCashSession"  # type: ignore
 
     id: int | None = Field(default=None, primary_key=True)
     booth_id: int = Field(foreign_key="Booth.id")
     user_id: int = Field(foreign_key="User.id")
     opened_at: datetime.datetime
-    initial_amount: decimal.Decimal | None = Field(sa_type=sa.Numeric())
-    closing_amount: decimal.Decimal | None = Field(sa_type=sa.Numeric())
+    initial_amount: decimal.Decimal | None = Field(sa_type=sa.Numeric())  # type: ignore
+    closing_amount: decimal.Decimal | None = Field(sa_type=sa.Numeric())  # type: ignore
     closing_reason: str | None
-    closing_observations: str | None = Field(sa_type=sa.Text())
+    closing_observations: str | None = Field(sa_type=sa.Text())  # type: ignore
     closed_at: datetime.datetime | None
-    created_at: datetime.datetime | None = Field(default_factory=lambda: datetime.now())
+    created_at: datetime.datetime | None = Field(
+        default_factory=lambda: datetime.datetime.now()
+    )
     created_by: int | None = Field(default=None, foreign_key="User.id")
     updated_at: datetime.datetime | None = Field(
         default=None, sa_column=Column(DateTime(), onupdate=func.now())
